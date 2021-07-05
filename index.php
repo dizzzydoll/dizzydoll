4c5df524173ae5f383a2c886c2172585c65f8bc9
kikikoswandi <kikikoswandi12@gmail.com>
2021-07-05 22:26:03 +0700

    website

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <head>
 	<title>dizzydoll</title>
 </head>
+<link rel=”icon” href=”img/ikon.png”>
         <link href="css/css1.css" rel="stylesheet">
         <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -14,16 +15,17 @@
      
 <ul>
   <li style="float: left; margin-top: -20px; margin-left: -20px;"><img src="img/logo.png" width="150" height="120" ></li>
-  <li><a href="#">about us </a></li>
-  <li><a href="#">contact</a></li>
-  <li><a href="#">product</a></li>
-  <li><a href="#">home</a></li>
+
+  <li style="margin-right: 80px;"><a href="#">about us </a></li>
+  <li style="margin-right: 80px;"><a href="#">contact</a></li>
+  <li style="margin-right: 80px;"><a href="#">product</a></li>
+  <li style="margin-right: 80px;"><a href="#">home</a></li>
 </ul>
 
 <div class="bacaan">
-	<h1>Yuk bermain dengan ku!</h1><br>
-	<p1>Kami menyediakan boneka hand made</p1><br>
-	<p1>dan berbagai produk hand made lainnya</p1><br>
+	<h1>Yuk bermain<br> dengan ku!</h1><br>
+	<p1>Kami menyediakan boneka handmade</p1><br>
+	<p1>dan berbagai produk handmade lainnya</p1><br>
 	<p1>yang sangat cocok untuk bermain anak</p1>
 
 </div>
