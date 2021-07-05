b4694e0dd55271423d454dadb3475f34636e7fab
kikikoswandi <kikikoswandi12@gmail.com>
2021-07-05 22:06:38 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>dizzydoll</title>
+</head>
+        <link href="css/css1.css" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Mali:bold,wght@6,clr@red, 700&display=swap" rel="stylesheet">
+    </head>
+    
+<body background="img/bg.jpg">     
+     
+<ul>
+  <li style="float: left; margin-top: -20px; margin-left: -20px;"><img src="img/logo.png" width="150" height="120" ></li>
+  <li><a href="#">about us </a></li>
+  <li><a href="#">contact</a></li>
+  <li><a href="#">product</a></li>
+  <li><a href="#">home</a></li>
+</ul>
+
+<div class="bacaan">
+	<h1>Yuk bermain dengan ku!</h1><br>
+	<p1>Kami menyediakan boneka hand made</p1><br>
+	<p1>dan berbagai produk hand made lainnya</p1><br>
+	<p1>yang sangat cocok untuk bermain anak</p1>
+
+</div>
+
+
+</body>
+</html>
